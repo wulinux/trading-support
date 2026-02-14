6702e38666bf6c2d49c4e731327c6cb5ea5520b0
wulinux <wulinux@users.noreply.github.com>
2026-02-14 18:29:40 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="wulinux"
+original_name="trading_support"
+original_urlname="trading-support"
+original_description="Awesome trading_support created by wulinux"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv trading_support $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
---
+++ trading_support/__main__.py
@@ -0,0 +1,6 @@
+"""Entry point for trading_support."""
+
+from trading_support.cli import main  # pragma: no cover
+
+if __name__ == "__main__":  # pragma: no cover
+    main()
